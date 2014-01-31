7098cb61da9ec03b00bc96e5204506f150fc53a5
doomidaniel <doomidaniel@yahoo.com>
2014-01-31 10:57:32 +0200

    P5

---
+++ Daniel/problem5.c
@@ -0,0 +1,16 @@
+//Problem 5
+#include <stdio.h>
+
+int main(void)
+{
+int i=2520;
+for(;;i+=20)
+{
+ if ( (i%11==0) && (i%13==0) && (i%14==0) && (i%16==0) && (i%17==0) && (i%18==0) && (i%19==0)  )
+     {
+         printf("%d\n",i);break;
+     }          
+}
+ system("pause");
+ return 0;
+}
